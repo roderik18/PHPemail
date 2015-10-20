6eb890e571bb12844430173ca5364ebfcb2bc52a
roderik18 <roderik@masure.org>
2015-10-20 15:57:59 +0200

    first git

---
+++ EmailFunction.php
@@ -0,0 +1,36 @@
+<?php
+// Preparation
+require_once('./vendor/autoload.php');
+use Postmark\Models\PostmarkAttachment;
+use Postmark\PostmarkClient;
+
+//function sendBatchOfEmails(){
+    $attachment = "test";
+    
+    //$TussenArray = $_POST;
+    //$message = $_POST;
+    $message = array(
+        //'To' => filter_input(INPUT_POST, 'To'),
+        //'From' => filter_input(INPUT_POST, 'From'),
+        //'Cc' => filter_input(INPUT_POST, 'Cc'),
+        //'Bcc' => filter_input(INPUT_POST, 'Bcc'),
+        //'Subject' => filter_input(INPUT_POST, 'Subject'),
+        //'TextBody' => filter_input(INPUT_POST, 'TextBody')
+        'To' => "roderikmasure@gmail.com",
+        'From' => "roderik@masure.org",
+        'Cc' => "mistermorgoth666@gmail.com",
+        'Subject' => "Test",
+        'TextBody' => "Dit is een testmail, ik hoop dat het nu eindelijk werkt."
+    );
+
+    $client = new PostmarkClient("f92ee11a-3de9-48ff-801e-1b6efc9afcdf");
+
+    $sendResult = $client->sendEmailBatch($message);
+    
+    // Did it send successfully?
+if( $sendResult != null ) {
+    echo 'The email was sent!';
+} else {
+    echo 'The email could not be sent!';
+}
+//}
---
+++ TestFunction.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+
+From: <?php echo $_POST["from"]; ?><br>
+to: <?php echo $_POST["to"]; ?><br>
+cc: <?php echo $_POST["cc"]; ?><br>
+bcc: <?php echo $_POST["bcc"]; ?><br>
+subject: <?php echo $_POST["subject"]; ?><br>
+text: <?php echo $_POST["text"]; ?><br>
+
+</body>
+</html>
